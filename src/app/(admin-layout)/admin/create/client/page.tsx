ab08a64951469bf03b7512c9b01726b65366156d
danielvichi <danielvichi@gmail.com>
2025-12-05 11:07:26 -0300

    Componentized BG

--- src/app/(admin-layout)/admin/create/client/page.tsx
+++ src/app/(admin-layout)/admin/create/client/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import PageBackground from '~/app/_components/page-background';
 import { useAdminAuthenticationContext } from '~/app/_contexts/admin-authentication-provider';
 import { CreateClientForm } from '~/app/_modules/create-client-form/create-client-form';
 import PageWrapperLayout from '~/app/_modules/headers/page-wrapper-layout';
@@ -16,7 +17,7 @@ export default function CreateClientPage() {
 
   return (
     <PageWrapperLayout className="p-8">
-      <div className="absolute top-0 right-0 bottom-0 left-0 bg-linear-to-br from-teal-300 via-blue-400 to-purple-600" />
+      <PageBackground />
       <div className="relative flex flex-col gap-8 w-full max-w-4xl justify-center py-8 px-4 md:px-8 bg-white">
         <CreateClientForm />
       </div>
---
+++ src/app/_components/page-background.tsx
@@ -0,0 +1,5 @@
+export default function PageBackground() {
+  return (
+    <div className="absolute top-0 right-0 bottom-0 left-0 bg-linear-to-br from-teal-300 via-blue-400 to-purple-600" />
+  );
+}
